1188780ac5372f6ccdffdf0a770d3a8f8e746b1f
Gilles Roudière <gilles.roudiere@gmail.com>
2018-07-07 16:20:19 +0200

    Add lap counter

--- level/level.gd
+++ level/level.gd
@@ -1,7 +1,10 @@
-extends Node2D
+extends Node
+
+func set_level_label(remaining, total):
+	$"GUI/LapsLabel".text=str(remaining) + "/" + str(total)
 
 func _ready():
-	pass
+	set_level_label(0, 100)
 
 func _on_AudioStreamPlayer_finished():
 	$Player.running = true
@@ -9,3 +12,6 @@ func _on_AudioStreamPlayer_finished():
 func _process(delta):
 	if (Input.is_action_just_pressed("ui_cancel")):
 		get_tree().quit()
+
+func _on_Level_lap_changed(lap):
+	set_level_label(lap, 100)
--- level/tilemap.gd
+++ level/tilemap.gd
@@ -1,16 +1,19 @@
 extends StaticBody2D
 
-signal level_done
+signal lap_changed
 
-export var number_of_laps = 3
+var lap = 0
 
-var laps = number_of_laps + 1
+var checkpoints = Array()
+var nb_of_checkpoints = 3
 
 func _on_Checkpoint_checkpoint(checkpoint_id):
-	# We should do better tests !
-	laps -= 1
-	
-	if (laps <= 0):
-		print("Level done !")
+	# Checkpoint logic
+	if (checkpoint_id == 0):
+		if (checkpoints == range(nb_of_checkpoints)):
+			lap += 1
+			emit_signal("lap_changed", lap)
+		checkpoints = [0]
 	else:
-		print(str(laps) + " laps remaining..." )
+		if (checkpoints == range(checkpoint_id)):
+			checkpoints = range(checkpoint_id+1)
